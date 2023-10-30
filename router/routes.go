1f62992acfd0eeed869f133a6d2253b15d32fc0e
Diego Czajka <diego.czajka@gmail.com>
2023-10-29 22:32:12 -0300

    feat dto response

--- router/routes.go
+++ router/routes.go
@@ -1,17 +1,17 @@
 package router
 
 import (
-	"github.com/diegoHDCz/gorental/handler"
+	"github.com/diegoHDCz/gorental/handler/categories"
 	"github.com/gin-gonic/gin"
 )
 
 func initializeRoutes(r *gin.Engine) {
 	v1 := r.Group("/api/v1")
 	{
-		v1.GET("/category", handler.ShowCategoryHandler)
-		v1.POST("/category", handler.CreateCategoryHandler)
-		v1.GET("/categories", handler.ListCategoriesHandler)
-		v1.PUT("/category", handler.UpdateCategoryHandler)
-		v1.DELETE("/category", handler.DeleteCategoryHandler)
+		v1.GET("/category", categories.ShowCategoryHandler)
+		v1.POST("/category", categories.CreateCategoryHandler)
+		v1.GET("/categories", categories.ListCategoriesHandler)
+		v1.PUT("/category", categories.UpdateCategoryHandler)
+		v1.DELETE("/category", categories.DeleteCategoryHandler)
 	}
 }
--- schemas/category.go
+++ schemas/category.go
@@ -1,6 +1,8 @@
 package schemas
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -8,4 +10,13 @@ type Category struct {
 	gorm.Model
 	Name  string
 	Image string
+}
+
+type CategoryResponse struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt time.Time `json:"deletedAt,omitempty"`
+	Name      string    `json:"name"`
+	Image     string    `json:"string"`
 }
